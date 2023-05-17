9d1430647d7ecf9202245e4e76ced27074d83827
Mitch Davis <mitchelldavis44@gmail.com>
2023-05-17 00:45:20 -0500

    initial commit

---
+++ awsprovider/awsprovider.go
@@ -0,0 +1,54 @@
+// awsprovider/aws_provider.go
+package awsprovider
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/yourusername/harmony/infrastructure"
+)
+
+type AWSProvider struct {
+	svc *ec2.EC2
+}
+
+func NewAWSProvider() infrastructure.Infrastructure {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+	}))
+
+	svc := ec2.New(sess)
+
+	return &AWSProvider{
+		svc: svc,
+	}
+}
+
+func (a *AWSProvider) CreateResource(name string) error {
+	// Use a.svc to create an EC2 instance, S3 bucket, etc.
+	// This is just a simplified example and won't actually work.
+	_, err := a.svc.RunInstances(&ec2.RunInstancesInput{
+		ImageId:      aws.String("ami-abc12345"),
+		InstanceType: aws.String("t2.micro"),
+		MinCount:     aws.Int64(1),
+		MaxCount:     aws.Int64(1),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *AWSProvider) DeleteResource(name string) error {
+	// Use a.svc to delete an EC2 instance, S3 bucket, etc.
+	// This is just a simplified example and won't actually work.
+	_, err := a.svc.TerminateInstances(&ec2.TerminateInstancesInput{
+		InstanceIds: []*string{aws.String(name)},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
